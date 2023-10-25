4a8d558fced4ab0c81bf75c6f2acda48abb2ed73
Charlotte Divine <divinemaina@gmail.com>
2023-10-25 10:10:00 +0200

    solve removeDuplicatesRecursive

--- src/exercise4.js
+++ src/exercise4.js
@@ -11,4 +11,20 @@ const sum = (arr) => {
 
      return arr[0]+sum(arr.slice(1))
 }
-console.log(sum([1,2,3]));
+console.log(sum([1, 2, 3]));
+
+// Implement a JavaScript function to remove duplicate elements from an array
+// const duplicate = (arr) => {
+//    return  [...new Set(arr)]
+    
+// }
+
+
+function removeDuplicatesRecursive(arr, index = 0, result = []) {
+    if (index === arr.length)  return result;
+    if (!result.includes(arr[index])) {
+        result.push(arr[index]);
+    }
+    return removeDuplicatesRecursive(arr, index + 1, result);
+}
+console.log(removeDuplicatesRecursive([1, 2, 1, 5, 2, 3]));
