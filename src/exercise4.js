2d9a1a0839c43902ebb1caa3199652af1ba2df70
Charlotte Divine <divinemaina@gmail.com>
2023-10-25 09:14:53 +0200

    solve sum with recoursion

--- src/exercise4.js
+++ src/exercise4.js
@@ -5,3 +5,10 @@ function prime(number_) {
     }
     return true;
 }
+//exercesise4.3
+const sum = (arr) => {
+    if(arr.length===0) return 0
+
+     return arr[0]+sum(arr.slice(1))
+}
+console.log(sum([1,2,3]));
