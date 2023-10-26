c59f27f27f9c4b36731b0c82de864ab8b89d5094
charlottedivine <divinemaina@gmail.com>
2023-10-26 11:56:40 +0200

    solve fibonnaci sequence

--- src/exercise5.js
+++ src/exercise5.js
@@ -2,3 +2,16 @@ function dec2bin(num) {
   return (num >>> 0).toString(2);
 }
 console.log(dec2bin(3));
+
+const fibonnaciSequence = (num)=>{
+  let fibonacci = [0,1];
+  let i = 2;
+  let nextFabonnacci = fibonacci[i-1] + fibonacci[i-2];
+  while(nextFabonnacci < num){
+    fibonacci[i] = nextFabonnacci;
+    i++;
+    nextFabonnacci = fibonacci[i-1] + fibonacci[i-2];
+  }
+  return fibonacci
+}
+console.log(fibonnaciSequence(5));
